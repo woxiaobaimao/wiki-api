0200a102b20f672930890c91d9ec468e7ec9bea9
woxiaobaimao <woxiaobaimao@163.com>
2024-04-23 16:50:35 +0800

    增加通用返回类型

--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -1,15 +1,20 @@
 package com.jiawa.wiki.service;
 
+import com.fasterxml.jackson.databind.util.BeanUtil;
 import com.jiawa.wiki.domain.Ebook;
 import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
 
+import com.jiawa.wiki.req.EbookReq;
+import com.jiawa.wiki.resp.EbookResp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -20,9 +25,17 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List list() {
-
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req ) {
+        EbookExample ebookExample=new EbookExample();
+        EbookExample.Criteria criteria=ebookExample.createCriteria();
+        criteria.andNameLike("%"+req.getName()+"%");
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        List<EbookResp> respList=new ArrayList<EbookResp>();
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp=new EbookResp();
+            BeanUtils.copyProperties(ebook, ebookResp);
+            respList.add(ebookResp);
+        }
+        return  respList;
     }
-
 }
