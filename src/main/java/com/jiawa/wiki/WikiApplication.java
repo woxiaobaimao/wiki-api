4db1d7054f5ce9df218e4577d98f0ccb0a6c7795
woxiaobaimao <woxiaobaimao@163.com>
2024-04-23 13:38:10 +0800

    test请求示例

--- src/main/java/com/jiawa/wiki/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/WikiApplication.java
@@ -1,8 +1,10 @@
 package com.jiawa.wiki;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.jiawa.wiki.mapper")
 @SpringBootApplication
 public class WikiApplication {
 
