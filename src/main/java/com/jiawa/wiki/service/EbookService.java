d69885960d7b7beb14f863dcf61565e907cdbe98
woxiaobaimao <woxiaobaimao@163.com>
2024-04-25 11:32:23 +0800

    异常处理

--- src/main/java/com/jiawa/wiki/service/EbookService.java
+++ src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -7,11 +7,13 @@ import com.jiawa.wiki.mapper.EbookMapper;
 
 import com.jiawa.wiki.req.EbookReq;
 import com.jiawa.wiki.resp.EbookResp;
+import com.jiawa.wiki.util.CopyUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -28,14 +30,10 @@ public class EbookService {
     public List<EbookResp> list(EbookReq req ) {
         EbookExample ebookExample=new EbookExample();
         EbookExample.Criteria criteria=ebookExample.createCriteria();
-        criteria.andNameLike("%"+req.getName()+"%");
-        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList=new ArrayList<EbookResp>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp=new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
+        if(!ObjectUtils.isEmpty(req.getName())){
+            criteria.andNameLike("%"+req.getName()+"%");
         }
-        return  respList;
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+        return CopyUtil.copyList(ebookList, EbookResp.class);
     }
 }
